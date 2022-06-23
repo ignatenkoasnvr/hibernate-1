fd125672b1f7a39e8a235717547f46c61d0b9b3b
Horider <horider@yandex.ru>
2022-06-23 16:29:19 +0300

    in process

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,12 +1,12 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
+import javax.persistence.NoResultException;
 import java.sql.SQLException;
-import java.util.List;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
@@ -15,20 +15,32 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User vasya = new User("User1", "Lastname1", "user1@mail.ru");
+      User petya = new User("User2", "Lastname2", "user2@mail.ru");
+      User olga = new User("User3", "Lastname3", "user3@mail.ru");
+      User sveta = new User("User4", "Lastname4", "user4@mail.ru");
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+      Car volvo = new Car("Audi", 1);
+      Car bmw = new Car("BMW", 2);
+      Car suzuki = new Car("Oka", 3);
+      Car lada = new Car("Lada", 4);
+
+      userService.add(vasya.setCar(volvo).setUser(vasya));
+      userService.add(petya.setCar(bmw).setUser(petya));
+      userService.add(olga.setCar(suzuki).setUser(olga));
+      userService.add(sveta.setCar(lada).setUser(sveta));
+
+      for (User user : userService.listUsers()) {
+         System.out.println(user + " " + user.getCar());
       }
 
+      System.out.println(userService.getCar("BMW", 2));
+
+      try {
+         User notFoundUser = userService.getCar("GAZ", 4211);
+      } catch (NoResultException e) {
+         System.out.println("User not found");
+      }
       context.close();
    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -6,5 +6,8 @@ import java.util.List;
 
 public interface UserService {
     void add(User user);
+
     List<User> listUsers();
+
+    User getCar(String model, int series);
 }
