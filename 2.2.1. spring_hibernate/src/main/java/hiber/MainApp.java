a8c23a4a4408b1e219ca7d7482d57369572f22f8
Минашкин Александр <aleksandrminashkin@rambler.ru>
2022-06-28 19:21:18 +0300

    completed the task

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -5,30 +5,29 @@ import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import javax.persistence.NoResultException;
 import java.sql.SQLException;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
+   public static void main(String[] args) {
       AnnotationConfigApplicationContext context = 
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
 
-      User vasya = new User("User1", "Lastname1", "user1@mail.ru");
-      User petya = new User("User2", "Lastname2", "user2@mail.ru");
-      User olga = new User("User3", "Lastname3", "user3@mail.ru");
-      User sveta = new User("User4", "Lastname4", "user4@mail.ru");
+      User User1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User User2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User User3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User User4 = new User("User4", "Lastname4", "user4@mail.ru");
 
-      Car volvo = new Car("Audi", 1);
-      Car bmw = new Car("BMW", 2);
-      Car suzuki = new Car("Oka", 3);
-      Car lada = new Car("Lada", 4);
+      Car Audi = new Car("Audi", 1);
+      Car BMW = new Car("BMW", 2);
+      Car Oka = new Car("Oka", 3);
+      Car Lada = new Car("Lada", 4);
 
-      userService.add(vasya.setCar(volvo).setUser(vasya));
-      userService.add(petya.setCar(bmw).setUser(petya));
-      userService.add(olga.setCar(suzuki).setUser(olga));
-      userService.add(sveta.setCar(lada).setUser(sveta));
+      userService.add(User1.setCar(Audi).setUser(User1));
+      userService.add(User2.setCar(BMW).setUser(User2));
+      userService.add(User3.setCar(Oka).setUser(User3));
+      userService.add(User4.setCar(Lada).setUser(User4));
 
       for (User user : userService.listUsers()) {
          System.out.println(user + " " + user.getCar());
@@ -36,11 +35,6 @@ public class MainApp {
 
       System.out.println(userService.getCar("BMW", 2));
 
-      try {
-         User notFoundUser = userService.getCar("GAZ", 4211);
-      } catch (NoResultException e) {
-         System.out.println("User not found");
-      }
       context.close();
    }
 }
